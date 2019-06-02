7dc86f4c895a6dc380c95c5b4945ce0d493754f7
Cangyu Lin <jefflin.je598@gmail.com>
2019-06-02 21:04:49 +0800

    新增中文註解

--- Sample1.java
+++ Sample1.java
@@ -1,13 +1,14 @@
+//宣告套件名稱
 package ya.Sample;
-
+//繼承前置檔案
 import android.app.Activity;
 import android.os.Bundle;
-
+//宣告主程式
 public class Sample1 extends Activity{
-	/*Called when the activity is first created*/
+	/*當新畫面(Activity)建立時自動生成*/
 	@Override
 	public void onCreate(Bundle saveInstanceState){
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		super.onCreate(savedInstanceState);	
+		setContentView(R.layout.main);	//新增版面
 	}
 }
