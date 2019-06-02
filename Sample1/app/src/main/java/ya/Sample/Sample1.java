52d02619d5ef30a21275b174613a16ee5f273bd2
Cangyu Lin <jefflin.je598@gmail.com>
2019-06-02 21:24:01 +0800

    2.1 Android應用程式

---
+++ Sample1/app/src/main/java/ya/Sample/Sample1.java
@@ -0,0 +1,13 @@
+package ya.Sample; //宣告套件
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sample1 extends AppCompatActivity {
+    /*以下為自動生成程式碼初始化*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main); //設定版面
+    }
+}
